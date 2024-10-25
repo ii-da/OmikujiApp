5f98f2a882c8515874d56998d5507fe25329002c
ii-da <kobi8.fuku@gmail.com>
2024-10-25 22:07:05 +0900

    リザルト画面レイアウト作成、TopActivityボタン

--- app/src/main/java/com/example/omikujiapp/TopActivity.java
+++ app/src/main/java/com/example/omikujiapp/TopActivity.java
@@ -1,6 +1,9 @@
 package com.example.omikujiapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class TopActivity extends AppCompatActivity {
+
+    private ImageView startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,15 @@ public class TopActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        startBtn = findViewById(R.id.start_btn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TopActivity.this, ResultActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
